ef3c9c469e7b0a75261ad13e359f6704f5157164
Patrick Leblanc <pat.leblanc@gmail.com>
2012-10-02 21:08:00 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassKeepr::Application.config.secret_token = 'cb885e60d03072fe441cd0e21288c6dcd669dfe996a8b1cfa756d7923a987f04fb012d6c4f2253a80ac273bfe8b54562e6c859004eb3ae3d8fc480fcaded815a'
